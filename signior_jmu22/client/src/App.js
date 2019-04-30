96d0fb9d18a161d25bef3e6f597435e47693d8b3
kevinxia787 <kevinxia787@gmail.com>
2019-04-30 01:19:07 -0400

    basic react app done with routing

---
+++ signior_jmu22/client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {  BrowserRouter as Router,  Route, Switch, Link} from 'react-router-dom';
+import Dashboard from './Dashboard/Dashboard';
+import Visualizations from './Visualizations/Visualizations';
+import Team from './Team/Team';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+import NavLink from 'react-bootstrap/NavLink';
+
+class App extends Component {  
+  render() {    
+    return (  
+      <Router>
+        <div>
+          <Navbar bg="light">
+            <Navbar.Brand>CS504 Dashboard</Navbar.Brand>
+            <Nav>
+              <LinkContainer to="/">
+                <NavLink>Home</NavLink>
+              </LinkContainer>
+              <LinkContainer to="/visualizations">
+                <NavLink>Visualizations</NavLink>
+              </LinkContainer>
+              <LinkContainer to="Team">
+                <NavLink>Team</NavLink>
+              </LinkContainer>
+            </Nav>
+          </Navbar>
+          <Switch>
+            <Route exact path='/' component={Dashboard}/>
+            <Route path='/visualizations' component={Visualizations}/>
+            <Route path='/team' component={Team}/>
+          </Switch>
+        </div> 
+      </Router>
+      
+      );  
+    }
+}
+
+export default App;
